59c5df6ca9af725c446412758c7b21f08a10d3a6
Kaltrina Mehmeti <bajramikaltrina@gmail.com>
2024-04-18 21:46:50 +0000

    Fixing the error in the URL file

--- musicfan/urls.py
+++ musicfan/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from hello_world import views as index_views
+from blog import views as index_views
 
 
 urlpatterns = [

e3d0540ce35a0569b92ba9a842de971175c7112a
Kaltrina Mehmeti <bajramikaltrina@gmail.com>
2024-04-24 21:15:51 +0000

    Updated the import in the admin file

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import Post
-from .models import ContactFormEntry
+from .models import Post, ContactFormEntry
 
 # Register your models here.
 admin.site.register(Post)
-
 admin.site.register(ContactFormEntry)
 

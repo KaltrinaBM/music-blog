ee2b16a6a382b94b2f99be39b62e9a9cfcf613bb
Kaltrina Mehmeti <bajramikaltrina@gmail.com>
2024-04-24 22:45:04 +0000

    Add comment model and register

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, ContactFormEntry
+from .models import Post, ContactFormEntry, Comment
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(ContactFormEntry)
+admin.site.register(Comment)
 
--- blog/models.py
+++ blog/models.py
@@ -29,4 +29,15 @@ class ContactFormEntry(models.Model):
         return f"{self.name} - {self.email}"  # Customize the string representation of the model instance
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+
 

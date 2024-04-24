13b75c48891814a1976badc638a4e82ff5f7ed79
Kaltrina Mehmeti <bajramikaltrina@gmail.com>
2024-04-24 20:19:36 +0000

    Added additional fields to Post model

--- musicfan/settings.py
+++ musicfan/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-kaltrinabm-musicblog-x3si8uy5acw.ws-eu110.gitpod.io', 
@@ -97,17 +97,18 @@ DATABASES = {
      'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
  }
 
+CLOUDINARY_STORAGE = {
+  'CLOUD_NAME': 'ydxm37gakc',
+  'API_KEY': '327847367919243',
+  'API_SECRET': 'QLPReZfX5HXB3zmswR8DoF8EZPY',
+}
+
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
     "https://*.herokuapp.com",
     "https://*.gitpod.io",
 ]
 
-CLOUDINARY_STORAGE = {
-  'CLOUD_NAME': 'ydxm37gakc',
-  'API_KEY': '327847367919243',
-  'API_SECRET': 'QLPReZfX5HXB3zmswR8DoF8EZPY',
-}
 
 
 # Password validation

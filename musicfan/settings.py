65ba2265d2bd32c7e1573df15f717f95abcfbc3c
Kaltrina Mehmeti <bajramikaltrina@gmail.com>
2024-04-26 00:58:58 +0000

    Fix Static Root

--- musicfan/settings.py
+++ musicfan/settings.py
@@ -158,7 +158,6 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'other_static_directory'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'

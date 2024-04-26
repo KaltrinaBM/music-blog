2efbec7eeb8cf47087e703d2079f0d17c8e4492e
Kaltrina Mehmeti <bajramikaltrina@gmail.com>
2024-04-26 01:16:56 +0000

    Add another static file

--- musicfan/settings.py
+++ musicfan/settings.py
@@ -160,7 +160,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+#STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

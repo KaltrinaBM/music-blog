e8604cf22d978ae6dc0180fe0bb25e2a0468b05a
Kaltrina Mehmeti <bajramikaltrina@gmail.com>
2024-04-27 00:07:26 +0000

    add comment form and message

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,7 @@
+from .models import Comment
 from django import forms
 from django.core.validators import EmailValidator
+
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=30)
@@ -7,3 +9,9 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100, required=False)  # Optional subject field
     message = forms.CharField(widget=forms.Textarea(attrs={'rows': 3, 'cols': 30}))
     phone_number = forms.CharField(max_length=15, required=False)  # Optional phone number field
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
